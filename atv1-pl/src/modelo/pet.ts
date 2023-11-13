f3a84dbc1f904e780e801e0d291711ce6eb05d28
WellingtonLFaria <wellingtonll.faria@gmail.com>
2023-11-13 16:11:00 -0300

    Mais algumas alterações na atualização de pet

--- atv1-pl/src/modelo/pet.ts
+++ atv1-pl/src/modelo/pet.ts
@@ -1,18 +1,23 @@
 export default class Pet {
-    private nome: string
-    private tipo: string
-    private raca: string
-    private genero: string
+    private nome: string;
+    private tipo: string;
+    private raca: string;
+    private genero: string;
 
     constructor(nome: string, raca: string, genero: string, tipo: string) {
-        this.nome = nome
-        this.raca = raca
-        this.genero = genero
-        this.tipo = tipo
-    }
+        this.nome = nome;
+        this.raca = raca;
+        this.genero = genero;
+        this.tipo = tipo;
+    };
 
-    public get getNome(){return this.nome}
-    public get getRaca(){return this.raca}
-    public get getGenero(){return this.genero}
-    public get getTipo(){return this.tipo}
+    public get getNome(){return this.nome};
+    public get getRaca(){return this.raca};
+    public get getGenero(){return this.genero};
+    public get getTipo(){return this.tipo};
+
+    public alterarNome(nome: string) {this.nome = nome};
+    public alterarRaca(raca: string) {this.raca = raca};
+    public alterarGenero(genero: string) {this.genero = genero};
+    public alterarTipo(tipo: string) {this.tipo = tipo};
 }
--- atv1-pl/src/negocio/atualizarPet.ts
+++ atv1-pl/src/negocio/atualizarPet.ts
@@ -1,5 +1,7 @@
 import Entrada from "../io/entrada";
 import Cliente from "../modelo/cliente";
+import CPF from "../modelo/cpf";
+import Pet from "../modelo/pet";
 import Atualizar from "./atualizar";
 
 export default class atualizarPet extends Atualizar {
@@ -14,6 +16,58 @@ export default class atualizarPet extends Atualizar {
     };
 
     public atualizar(): void {
-        
-    }
-}
+        let cpf = this.entrada.receberTexto(`Por favor, informe o número do CPF do cliente a ser atualizado: `);
+        this.clientes.forEach(cliente => {
+            if (cliente.getCpf.getValor == cpf) {
+                if (cliente.getPets.length == 0) {
+                    console.log(`O cliente ${cliente.nome} ainda não possui pets.`);
+                } else {
+                    console.log(`O cliente ${cliente.nome} possui os seguintes pets:`);
+                    cliente.getPets.forEach(pet => {
+                        console.log(`[${cliente.getPets.indexOf(pet)}] ${pet.getNome}`);
+                    });
+                    console.log(`Escolha o pet para atualizar as informações: `);
+                    let indexPet = this.entrada.receberNumero(``);
+                    let pet = cliente.getPets[indexPet];
+                    let execucao = true;
+                    while (execucao) {
+                        console.log(`[1] Atualizar nome\n[2] Atualizar raça\n[3] Atualizar gênero\n[4] Atualizar tipo\n[0] Finalizar alterações`);
+                        let opcao = this.entrada.receberNumero(``);
+                        switch (opcao) {
+                            case 0:
+                                execucao = false;
+                                break;
+                            case 1:
+                                let nome = this.entrada.receberTexto(`Informe o nome do Pet: `);
+                                pet.alterarNome(nome);
+                                break;
+                            case 2:
+                                let raca = this.entrada.receberTexto(`Informe a raça do Pet: `);
+                                pet.alterarRaca(raca);
+                                break;
+                            case 3:
+                                let genero = this.entrada.receberTexto(`Informe o gênero do Pet: `);
+                                pet.alterarGenero(genero);
+                                break;
+                            case 4:
+                                let tipo = this.entrada.receberTexto(`Informe o tipo do Pet: `);
+                                pet.alterarTipo(tipo);
+                                break;
+                            default:
+                                console.log("Opção inválida!");
+                        };
+                    };
+                };
+            };
+        });
+    };
+};
+
+// Testes
+let clientes = [new Cliente("Wellington", "Wellington", new CPF("12345678912", new Date())), new Cliente("Wellington", "Wellington", new CPF("12345678913", new Date())), new Cliente("Wellington", "Wellington", new CPF("12345678914", new Date())), new Cliente("Wellington", "Wellington", new CPF("12345678915", new Date()))]
+
+clientes[2].adicionarPet(new Pet("Luna", "Shitzu", "Femea", "Cachorro"));
+clientes[3].adicionarPet(new Pet("Luna", "Shitzu", "Femea", "Cachorro"));
+clientes[0].adicionarPet(new Pet("Luna", "Shitzu", "Femea", "Cachorro"));
+
+new atualizarPet(clientes, new Entrada()).atualizar();
